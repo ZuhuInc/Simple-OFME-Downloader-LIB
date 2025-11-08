f22b728372a45114d769ad9cf5e9406863970fef
Turfboy27 <235756415+Turfboy27@users.noreply.github.com>
2025-11-08 22:45:51 +0100

    Refactor driver setup and game version extraction
    Replaces manual Brave browser setup with seleniumbase Driver for improved reliability. Refactors game version extraction logic for 'online-fix.me' and 'steamrip.com' origins, streamlining login and captcha handling, and improves version string normalization.

--- OFME-VC.py
+++ OFME-VC.py
@@ -7,13 +7,11 @@ import time
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import requests
+from seleniumbase import Driver
 
-brave_path = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
 username = input("Enter your username: ")
 password = input("Enter your password: ")
-options = Options()
-options.binary_location = brave_path
-driver = webdriver.Chrome(options = options)
+driver = Driver(browser='brave', uc = True)
 
 url = "https://raw.githubusercontent.com/ZuhuInc/Simple-OFME-Downloader-LIB/main/Download-DB.txt"
 response = requests.get(url)
@@ -50,37 +48,47 @@ if current_game:
 counter = 0
 game_update = {}
 for game in games:
-    time.sleep(1)
     if 'https://online-fix.me/' in game['Origin']:
+        driver.get(game['Origin'])
+        if counter == 0:
+            search = driver.find_element(By.NAME, 'login_name')
+            search.send_keys(username)
+            search = driver.find_element(By.NAME, 'login_password')
+            search.send_keys(password)
+            time.sleep(0.5)
+            search.send_keys(Keys.RETURN)
+            counter += 1
+            pass
+        time.sleep(0.5)
+        try:
+            select_version = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "quote")))
+            version = select_version.find_element(By.TAG_NAME, 'b').text.replace('Версия игры: ', '')
+        except:
+            print("Version not found")
+
+    elif 'https://steamrip.com/' in game['Origin']:
+        url = game['Origin']
+        driver.uc_open_with_reconnect(url,4)
+        driver.uc_gui_click_captcha()
+        version = driver.find_element(By.TAG_NAME, 'h1').text.split('(')[1].replace(')','')
         pass
     else:
         continue
-    
-    driver.get(game['Origin'])
-    if counter == 0:
-        search = driver.find_element(By.NAME, 'login_name')
-        search.send_keys(username)
-        search = driver.find_element(By.NAME, 'login_password')
-        search.send_keys(password)
-        time.sleep(1)
-        search.send_keys(Keys.RETURN)
-        counter += 1
-        pass
-    time.sleep(1)
-    try:
-        select_version = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "quote")))
-        version = select_version.find_element(By.TAG_NAME, 'b').text.replace('Версия игры: ', '')
-    except:
-        print("Version not found")
 
     if "Build" in version:
         version = version.replace("Build ", "")
+    
+    if "v" in version:
+        version = version.replace("v", "")
 
     print(f"{game['Name']}: {version} (Current: {game['Version']})")  
 
     if version != game['Version']:
         game_update[game['Name']] = version, game['Version']
 
+
 driver.quit()
 print(game_update)
+    
+
